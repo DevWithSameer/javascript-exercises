2767a6d4f06a3bc68ff4139625aed840fd8bc8ea
Sameer Ahmed <48552405+Sameer-Ahmed01@users.noreply.github.com>
2022-09-29 21:20:19 +0500

    Create 18.Seeing_the_World.js

---
+++ 18.Seeing_the_World.js
@@ -0,0 +1,20 @@
+// 18. Seeing the World: Think of at least five places in the world you’d like to visit.
+
+let placesToVisit = ["Rome", "London", "Maui", "Paris", "London"];
+console.log(placesToVisit);
+let alphabetical = Array.from(placesToVisit);
+alphabetical.sort();
+console.log(placesToVisit);
+console.log(alphabetical);
+let reverse_alphabetical = Array.from(placesToVisit);
+reverse_alphabetical.reverse();
+console.log(placesToVisit);
+console.log(reverse_alphabetical);
+placesToVisit.reverse();
+console.log(placesToVisit);
+placesToVisit.reverse();
+console.log(placesToVisit);
+placesToVisit.sort();
+console.log(placesToVisit);
+placesToVisit.reverse();
+console.log(placesToVisit);
