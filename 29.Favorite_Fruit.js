70a5b25c67fdcd91992530896e847fded99ce1de
Sameer Ahmed <48552405+Sameer-Ahmed01@users.noreply.github.com>
2022-09-29 22:13:35 +0500

    Update 29.Favorite_Fruit.js

--- 29.Favorite_Fruit.js
+++ 29.Favorite_Fruit.js
@@ -6,17 +6,13 @@ let favoriteFruits = ["Pomegranate", "Apple", "Orange"];
 for (let i = 0; i < favoriteFruits.length; i++) {
   if ("Banana" == favoriteFruits[i]) {
     console.log("I Really like Apples!");
-  } 
-  else if ("Pomegranate" == favoriteFruits[i]) {
+  } else if ("Pomegranate" == favoriteFruits[i]) {
     console.log("I Really like Pomegranates!");
-  } 
-  else if ("Kiwi" == favoriteFruits[i]) {
+  } else if ("Kiwi" == favoriteFruits[i]) {
     console.log("I Really like Apples!");
-  }
-  else if ("Strawberries" == favoriteFruits[i]) {
+  } else if ("Strawberries" == favoriteFruits[i]) {
     console.log("I Really like Apples!");
-  }
-  else if ("Raspberries	" == favoriteFruits[i]) {
+  } else if ("Raspberries	" == favoriteFruits[i]) {
     console.log("I Really like Apples!");
   }
 }
