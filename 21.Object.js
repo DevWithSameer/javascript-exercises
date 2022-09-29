e756c53f29e0d9d476b79815974a1c36c3a8f875
Sameer Ahmed <48552405+Sameer-Ahmed01@users.noreply.github.com>
2022-09-29 21:34:09 +0500

    Create 21.Object.js

---
+++ 21.Object.js
@@ -0,0 +1,8 @@
+// 21. They think of something you could store in a JavaScript Object. Write a program that creates Objects containing these items.
+
+const Student = {
+  name: "Sameer",
+  Age: 24,
+  Course: "Metaverse"
+};
+console.log(Student);
