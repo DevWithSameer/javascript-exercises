1ae7f3f3deb9a0aebae4d98dba8b1fd6645e729a
Sameer Ahmed <48552405+Sameer-Ahmed01@users.noreply.github.com>
2022-09-29 21:30:05 +0500

    Create 20.Array.js

---
+++ 20.Array.js
@@ -0,0 +1,9 @@
+// 20. Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let mountains = ["Denali", "Mount Kilimanjaro"];
+let rivers = ["Nile", "Amazon", "Chang Jiang"];
+let countries = ["Germany", "Turkey", "Iran"];
+let cities = ["Berlin", "Istanbul", "Tabraiz"];
+let languages = ["Urdu", "English"];
+let list = mountains.concat(rivers, countries, cities, languages);
+console.log(list);
