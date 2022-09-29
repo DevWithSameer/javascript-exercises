32ba2fe4089416c22197c9c01a52cad96f0caec9
Sameer Ahmed <48552405+Sameer-Ahmed01@users.noreply.github.com>
2022-09-29 22:05:13 +0500

    Create 27.Alien_Colors#3.js

---
+++ 27.Alien_Colors#3.js
@@ -0,0 +1,34 @@
+// 27. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+
+let alienColor="green"
+if(alienColor=="green"){
+    console.log("Yahooo you get 5 points")
+}
+else if (alienColor=="yellow"){
+    console.log("earned 10 points");
+}
+else{
+    console.log("earned 15 points");
+}
+
+alienColor="yellow"
+if(alienColor=="green"){
+    console.log("Yahooo you get 5 points")
+}
+else if (alienColor=="yellow"){
+    console.log("earned 10 points");
+}
+else{
+    console.log("earned 15 points");
+}
+
+alienColor="red"
+if(alienColor=="green"){
+    console.log("Yahooo you get 5 points")
+}
+else if (alienColor=="yellow"){
+    console.log("earned 10 points");
+}
+else{
+    console.log("earned 15 points");
+}
