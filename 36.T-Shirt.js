12bbfdc0e263a3d0a567710c42d1f9aaead621cc
Sameer Ahmed <48552405+Sameer-Ahmed01@users.noreply.github.com>
2022-09-29 22:33:59 +0500

    Create 36.T-Shirt.js

---
+++ 36.T-Shirt.js
@@ -0,0 +1,8 @@
+// 36. T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+function makeShirt(size, msg) {
+  console.log(
+    "Size of T-Shirt is:" + size + " message printed on it is:" + msg
+  );
+}
+makeShirt("large", " Young Realities Unreal Pleasure");
