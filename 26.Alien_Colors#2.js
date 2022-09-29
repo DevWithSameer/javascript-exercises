988afe2f6eed3d64f5e9479bd98cd26e8ddaee37
Sameer Ahmed <48552405+Sameer-Ahmed01@users.noreply.github.com>
2022-09-29 21:59:31 +0500

    Create 26.Alien_Colors#2.js

---
+++ 26.Alien_Colors#2.js
@@ -0,0 +1,7 @@
+// 26. Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+
+let alienColor = "green";
+console.log(alienColor == "green" ? "Yahooo you get 5 points" : "earned 10 points");
+
+alienColor = "yellow";
+console.log(alienColor == "green" ? "Yahooo you get 5 points" : "earned 10 points");
