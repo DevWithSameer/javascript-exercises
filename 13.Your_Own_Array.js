bf579adbd3d7d484b25073e033c3987f9a3ed2a7
Sameer Ahmed <48552405+Sameer-Ahmed01@users.noreply.github.com>
2022-09-29 16:05:58 +0500

    Create 13.Your_Own_Array.js

---
+++ 13.Your_Own_Array.js
@@ -0,0 +1,9 @@
+// 13. Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+const vehicle = "Bus";
+let list = [
+  "I would like to own a " + vehicle,
+  "I like to clean my " + vehicle,
+  "I love to drive my " + vehicle,
+];
+console.log(list);
