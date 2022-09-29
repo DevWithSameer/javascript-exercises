ca74c9b8713135d89df933b9b77e8b0fa6b83128
Sameer Ahmed <48552405+Sameer-Ahmed01@users.noreply.github.com>
2022-09-29 21:49:05 +0500

    Create 23.Conditional_Tests.js

---
+++ 23.Conditional_Tests.js
@@ -0,0 +1,9 @@
+// 23. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+let Class = 4;
+console.log(Class === 4 ? "Class is 4" : Class);
+console.log(Class >= 5 ? "Matric Student" : "Primary Student");
