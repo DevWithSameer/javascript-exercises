82aa8727e24a40c39627fd6c2f4a620165256b6c
Sameer Ahmed <48552405+Sameer-Ahmed01@users.noreply.github.com>
2022-09-29 21:59:28 +0500

    Create 25.Alien_Colors#1.js

---
+++ 25.Alien_Colors#1.js
@@ -0,0 +1,6 @@
+// 25. Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+
+let alienColor = "green";
+console.log(alienColor == "green" ? "Yahooo you get 5 points" : "");
+alienColor = "Yellow";
+console.log(alienColor == "green" ? "Yahooo you get 5 points" : "");
